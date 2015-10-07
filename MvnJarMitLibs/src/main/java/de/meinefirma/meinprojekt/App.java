660549c4041aaa39a506821eea70b47b414e429f
norbert <norbert.jahn@aioinissaydowa.eu>
2015-10-07 08:54:10 +0200

    Einarbeiten Git Tutorial -> Commit Project

---
+++ MvnJarMitLibs/src/main/java/de/meinefirma/meinprojekt/App.java
@@ -0,0 +1,15 @@
+package de.meinefirma.meinprojekt;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class App 
+{
+	private static final Logger LOGGER = Logger.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+    	PropertyConfigurator.configure("C:/Software NJA/ws_mars/MvnJarMitLibs/target/log4j.properties");
+    	LOGGER.info("--- Hallo Logger! ---");
+    }
+}
---
+++ MvnJarMitLibs/src/test/java/de/meinefirma/meinprojekt/AppTest.java
@@ -0,0 +1,13 @@
+package de.meinefirma.meinprojekt;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+	public void testApp() {
+		App.main(null);
+	}
+}
