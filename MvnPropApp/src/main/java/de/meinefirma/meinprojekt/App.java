8f1ff34f59d84d52c85aa3f22a1704ece88a0481
norbert <norbert.jahn@aioinissaydowa.eu>
2015-10-08 09:16:40 +0200

    Start

---
+++ MvnPropApp/src/main/java/de/meinefirma/meinprojekt/App.java
@@ -0,0 +1,12 @@
+package de.meinefirma.meinprojekt;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
