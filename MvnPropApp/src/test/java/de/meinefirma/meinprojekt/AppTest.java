23c0f817691cb2167d761b8313789c8ff2ef184d
norbert <norbert.jahn@aioinissaydowa.eu>
2015-10-08 10:28:14 +0200

    Lauffähige Version

--- MvnPropApp/src/test/java/de/meinefirma/meinprojekt/AppTest.java
+++ MvnPropApp/src/test/java/de/meinefirma/meinprojekt/AppTest.java
@@ -8,19 +8,19 @@ import junit.framework.TestCase;
  * Unit test for simple App.
  */
 public class AppTest extends TestCase {
-	
+
 	public void testApp() {
-		
+
 		Properties propsApp = new Properties(), propsTst = new Properties();
 		try {
-			propsApp.load(getClass().getResourceAsStream( "/filtered/MeineProps.properties"));
-			propsTst.load(getClass().getResourceAsStream( "/filtered/MeineTestProps.properties"));
-			
-		} catch(IOException ex ) {
+			propsApp.load(getClass().getResourceAsStream("/filtered/MeineProps.properties"));
+			propsTst.load(getClass().getResourceAsStream("/MeineTestProps.properties"));
+
+		} catch (IOException ex) {
 			throw new RuntimeException(ex.getCause());
 		}
-		assertEquals( "MeinPomProp vor Merge",  "PropAusPom",       propsApp.get( "MeinPomProp"));
+		assertEquals("MeinPomProp vor Merge", "PropAusPom", propsApp.get("MeinPomProp"));
 		propsApp.putAll(propsTst);
-		assertEquals( "MeinPomProp nach Merge", "PropAusTestProps", propsApp.get( "MeinPomProp" ) );
+		assertEquals("MeinPomProp nach Merge", "PropAusTestProps", propsApp.get("MeinPomProp"));
 	}
 }
