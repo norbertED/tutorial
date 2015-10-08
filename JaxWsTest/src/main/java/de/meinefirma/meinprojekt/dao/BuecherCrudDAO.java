382bad83bfd12379d7c239eca95134732cd01d26
norbert <norbert.jahn@aioinissaydowa.eu>
2015-10-08 14:55:59 +0200

    Project Start

---
+++ JaxWsTest/src/main/java/de/meinefirma/meinprojekt/dao/BuecherCrudDAO.java
@@ -0,0 +1,14 @@
+package de.meinefirma.meinprojekt.dao;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import de.meinefirma.meinprojekt.ws.*;
+
+public class BuecherCrudDAO {
+
+	// DAO (Data Access Object) für CRUD (Create, Read, Update, Delete)
+	public BuecherCrudDAO() {
+		
+	}
+
+}
